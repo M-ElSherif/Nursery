3e2e83ac84471f0e5f038abd62789d86bcf5eb6f
Mohamed El Sherif <mohamed.elsherif90@gmail.com>
2021-01-02 15:34:55 -0500

    feat: expenseDAO created and expense entity
    - ExpenseDAO create
    - Expense entity created
    - Updated DAO classes to create and close a DB connection in each of their methods

--- DAO/ExpenseDAO.py
+++ DAO/ExpenseDAO.py
@@ -1,25 +1,27 @@
 from DAO.AbstractDAO import AbstractDAO
 from Database.DBManager import DBManager
-from Entities.Student import Student
+from Entities.Expense import Expense
 
 
-class StudentDAO(AbstractDAO):
+class ExpenseDAO(AbstractDAO):
 
     def __init__(self):
         self.sql_connection = None
 
-    def create(self, student: Student) -> bool:
+    def create(self, expense: Expense) -> bool:
         cur = None
 
-        name = student.name
-        grade = student.grade
-        age = student.age
+        category = expense.category
+        description = expense.description
+        exp_type = expense.exp_type
+        cost = expense.cost
+        date = expense.date
 
         try:
             self.sql_connection = DBManager.create_connection()
             cur = self.sql_connection.cursor()
-            cur.execute("INSERT INTO students (name, grade, age) VALUES (?,?,?)",
-                        (name, grade, age))
+            cur.execute("INSERT INTO expenses (category, description, type, cost, date) VALUES (?,?,?,?,?)",
+                        (category, description, exp_type, cost, date))
             self.sql_connection.commit()
             cur.close()
             self.sql_connection.close()
@@ -31,32 +33,34 @@ class StudentDAO(AbstractDAO):
             self.sql_connection.close()
             return False
 
-    def read(self, student_id) -> Student:
+    def read(self, expense_id: int) -> Expense:
         cur = None
 
         try:
             self.sql_connection = DBManager.create_connection()
             cur = self.sql_connection.cursor()
-            cur.execute("SELECT studentID,name,grade,age "
-                        "FROM students "
-                        "WHERE studentID = ?", (student_id,))
+            cur.execute("SELECT expenseID, category, description, type, cost, date "
+                        "FROM expenses "
+                        "WHERE expenseID = ?", (expense_id,))
             row = cur.fetchone()
             cur.close()
             self.sql_connection.close()
-            return Student(row[1], row[2], row[3])
+            return Expense(row[1], row[3], row[4], row[5], row[2])
         except Exception as e:
             print(e)
             self.sql_connection.close()
 
-    def update(self, student, student_id) -> bool:
+    def update(self, expense: Expense, expense_id: int) -> bool:
         cur = None
 
         try:
             self.sql_connection = DBManager.create_connection()
             cur = self.sql_connection.cursor()
-            cur.execute("UPDATE students "
-                        "SET name= ?, grade = ?, age = ? "
-                        "WHERE studentID = ?", (student.name, student.grade, student.age, student_id))
+            cur.execute("UPDATE expenses "
+                        "SET category = ?, description = ?, type = ?, cost = ?, date = ? "
+                        "WHERE expenseID = ?", (
+                            expense.category, expense.description, expense.exp_type, expense.cost, expense.date,
+                            expense_id))
             self.sql_connection.commit()
             self.sql_connection.close()
             return True
@@ -68,13 +72,13 @@ class StudentDAO(AbstractDAO):
         self.sql_connection.close()
         return False
 
-    def delete(self, student_id) -> bool:
+    def delete(self, expense_id: int) -> bool:
         cur = None
 
         try:
             self.sql_connection = DBManager.create_connection()
             cur = self.sql_connection.cursor()
-            cur.execute("DELETE FROM students WHERE studentID = ?", (student_id,))
+            cur.execute("DELETE FROM expenses WHERE expenseID = ?", (expense_id,))
             self.sql_connection.commit()
             self.sql_connection.close()
             return True
@@ -92,7 +96,7 @@ class StudentDAO(AbstractDAO):
         try:
             self.sql_connection = DBManager.create_connection()
             cur = self.sql_connection.cursor()
-            cur = self.sql_connection.execute('SELECT name,age,grade FROM students')
+            cur = self.sql_connection.execute('SELECT category, description, type, cost, date FROM expenses')
             fields = [description[0] for description in cur.description]
             cur.close()
             self.sql_connection.close()
